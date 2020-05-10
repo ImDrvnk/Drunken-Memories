0d670dc865b938c5e78b5bf6e5e11d6998a697a8
@matjan98 <matjan98@gmail.com>
2020-05-10 23:08:54 +0200

    funkcje

---
+++ Funkcja.py
@@ -0,0 +1,21 @@
+def funkcja():
+    # tu jest funkcja ktora cos robi, np:
+    print("dzingiel")
+    print("*")
+    print("*")
+    print("*")
+    print("*")
+    print("*")
+
+
+def funkcja_1():
+    zmienna = 123
+    return "jakis tekst"
+
+
+
+def funkcja_z_argumentem(argument):
+    print(argument)
+
+def dodaj(jedna_liczba, druga_liczba):
+    return jedna_liczba + druga_liczba
